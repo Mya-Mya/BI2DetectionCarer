7b8432a27410c13f9fa295b45cd5c40fab5db949
Mya-Mya <39019907+Mya-Mya@users.noreply.github.com>
2020-03-31 15:34:56 +0900

    キーイベントはここが受け取るように

--- src/view/MasterView.java
+++ src/view/MasterView.java
@@ -4,12 +4,16 @@ import ui.ColorFactory;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class MasterView extends JFrame {
+public class MasterView extends JFrame implements KeyListener {
     public static MasterView inst;
+    private JPanel scene;
 
     public MasterView() {
         super("BI2DetectionInspector");
+        addKeyListener(this);
         setPreferredSize(new Dimension(1000, 700));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setBackground(ColorFactory.back);
@@ -17,13 +21,30 @@ public class MasterView extends JFrame {
 
         pack();
         setVisible(true);
+        setFocusable(true);
         inst = this;
         inst.changeScene(new InitialSettingView());
     }
 
     public void changeScene(JPanel scene) {
         getContentPane().removeAll();
+        this.scene=scene;
         getContentPane().add(scene);
         validate();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        if(scene instanceof KeyListener)((KeyListener) scene).keyTyped(e);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(scene instanceof KeyListener)((KeyListener) scene).keyPressed(e);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if(scene instanceof KeyListener)((KeyListener) scene).keyReleased(e);
     }
 }
