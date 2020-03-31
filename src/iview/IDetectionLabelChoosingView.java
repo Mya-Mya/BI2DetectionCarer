177bcce6c26d8230ec74bdcdeaaf26f2740c45dd
Mya-Mya <interleukin413@gmail.com>
2020-03-31 14:32:51 +0900

    Inspecting画面へ遷移の際に必要な引数を追加

--- src/iview/IDetectionLabelChoosingView.java
+++ src/iview/IDetectionLabelChoosingView.java
@@ -1,5 +1,7 @@
 package iview;
 
+import model.Inspection;
+
 import java.util.List;
 
 /**
@@ -8,5 +10,5 @@ import java.util.List;
  */
 public interface IDetectionLabelChoosingView {
     void setLabelTextList(List<String> labelList);
-    void gotoInspectingView();
+    void gotoInspectingView(Inspection inspection, String targetLabel);
 }
