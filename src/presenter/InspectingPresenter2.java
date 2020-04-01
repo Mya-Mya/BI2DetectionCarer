e78e34c51235d4b2abece6e8f4f1ce6e12cd1cc8
Mya-Mya <39019907+Mya-Mya@users.noreply.github.com>
2020-04-01 09:50:48 +0900

    新しいInspecting画面を提案

---
+++ src/presenter/InspectingPresenter2.java
@@ -0,0 +1,11 @@
+package presenter;
+
+import iview.IInspectingView;
+import model.Inspection;
+
+public class InspectingPresenter2 {
+    private IInspectingView view;
+    public InspectingPresenter2(Inspection inspection,String targetLabel){
+
+    }
+}
---
+++ src/view/InspectingView2.java
@@ -0,0 +1,39 @@
+package view;
+
+import iview.IInspectingView;
+import model.ImageInfo;
+import model.Inspection;
+
+import javax.swing.*;
+
+public class InspectingView2 extends JPanel implements IInspectingView {
+    @Override
+    public void setTargetLabelName(String label) {
+
+    }
+
+    @Override
+    public void addImage(ImageInfo image) {
+
+    }
+
+    @Override
+    public void onInspectedAllPair() {
+
+    }
+
+    @Override
+    public void gotoDetectionLabelChoosingView(Inspection inspection) {
+
+    }
+
+    @Override
+    public void setLabelKeybindMessage(String labelKeybindMessage) {
+
+    }
+
+    @Override
+    public void removeImage(ImageInfo imageInfo) {
+
+    }
+}
